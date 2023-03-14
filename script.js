36dc8a6c06ee5942544f191c21c79da2caa2ae00
Slyvred <remihage2003@gmail.com>
2023-03-14 14:31:50 +0100

    avancement sur la calculatrice et sécurisation du calcul

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
-function printChar()
-{
-    document.getElementById("")
+let liste = [];
+let allNumber = "";
+
+function addToList(arg) {
+  if (arg === "+" || arg === "-" || arg === "*" || arg === "/") {
+    liste.push(" " + arg + " "); // " " est notre séparateur
+  } else {
+    liste.push(arg); // juste notre chiffre
+  }
+  document.getElementById("result").innerHTML = liste.join(""); // Affiche l'opération à l'écran
+}
+
+// Vérifie que notre syntaxe est correcte e.g. pas 1..1 / 2
+function secureEval(expression) {
+  try {
+    return eval(expression);
+  } catch (error) {
+    return new Error("Syntaxe");
+  }
+}
+
+function joinList() {
+  allNumber = liste.join("");
+}
+
+function calc(listenumber) {
+  joinList(listenumber);
+  document.getElementById("result").innerHTML = secureEval(allNumber);
+  document.getElementById("calcul").innerHTML = allNumber;
+  liste = [];
+}
+
+function del() {
+  liste.pop();
+  document.getElementById("result").innerHTML = liste.join("");
+}
+
+function reset() {
+  liste = [];
+  document.getElementById("result").innerHTML = 0;
+  document.getElementById("calcul").innerHTML = "";
 }
