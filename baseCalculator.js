8e4399aa69841e256e42d94eb4fd0161a85ad1a3
Slyvred <remihage2003@gmail.com>
2023-03-28 19:27:18 +0200

    fixes divers

--- baseCalculator.js
+++ baseCalculator.js
@@ -4,6 +4,7 @@ class BaseCalculator {
     this.prec = [];
     this.allNumber = "";
     this.start, this.end, this.time;
+    this.timeIcon = "<i class='fa-regular fa-hourglass'></i>";
   }
 
   // Ajoute l'input à la liste et à l'écran
@@ -37,6 +38,7 @@ class BaseCalculator {
 
   calc() {
     // On traite notre chaîne pour la rendre lisible par eval()
+
     this.joinList();
     console.log(this.liste);
     console.log(this.allNumber);
@@ -44,6 +46,7 @@ class BaseCalculator {
     document.getElementById("result").innerHTML = this.secureEval(
       this.allNumber
     ); // Affiche le résultat
+
     document.getElementById("calcul").innerHTML = this.allNumber; // Affiche le calcul qu'on vient de faire
 
     this.prec = []; // On supprime l'opération précédente
@@ -55,16 +58,21 @@ class BaseCalculator {
     this.time = (this.end - this.start) / 1000;
     console.log(`Temps d'opération: ${this.time}s`);
     document.title = `Temps d'opération: ${this.time}s`;
+    document.getElementById("time").innerHTML =
+      this.timeIcon + "\t" + this.time + "s";
   }
 
   // Supprime l'input qui vient d'être fait
   del() {
-    if (document.getElementById("result").innerHTML == "") { // Si jamais on appuie deux fois sur del après avoir fini un calcul
-      document.getElementById("result").innerHTML = document.getElementById("calcul").innerHTML; // Notre résultat précédent devient le courant
+    if (document.getElementById("result").innerHTML == "") {
+      // Si jamais on appuie deux fois sur del après avoir fini un calcul
+      document.getElementById("result").innerHTML =
+        document.getElementById("calcul").innerHTML; // Notre résultat précédent devient le courant
       document.getElementById("calcul").innerHTML = ""; // On efface ce qu'on avait avant
       this.liste = this.prec; // On remplace la liste des arguments
-    }
-    else { // Sinon, on supprime juste le dernier caractère
+      this.start = performance.now(); // C'est un nouveau calcul, il ne faut pas oublier de reset le timer
+    } else {
+      // Sinon, on supprime juste le dernier caractère
       this.liste.pop();
       document.getElementById("result").innerHTML = this.liste.join("");
     }
@@ -78,4 +86,4 @@ class BaseCalculator {
   }
 }
 
-var calculator = new BaseCalculator();
+const calculator = new BaseCalculator();
